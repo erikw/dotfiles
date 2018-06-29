68463cc4be35e70c23eb20be09a1814e5c585dbb
Erik Westrup <erik.westrup@gmail.com>
2018-06-29 11:58:06 +0200

    Collected changes
    Former-commit-id: 8ef1463a586cfa691c1157f0d39a37458e3ce8b8
    Former-commit-id: d5d258468105b58655c16ac6376f790a5982afc2

--- bin/rename_sane.sh
+++ bin/rename_sane.sh
@@ -11,6 +11,8 @@ IFS=$'\n'
 for dir in $(find $path -type d | tac); do
 	echo "$dir"
 	cd "$dir"
+	# TODO on macOS, case-only rename is not allowed. Need to rename file to temporay name first.
+	# https://stackoverflow.com/questions/7787029/how-do-i-rename-all-files-to-lowercase
 	rename.pl 's/(.*)/lc($1)/e' *
 	rename.pl 's/ /_/g' *
 	cd - >/dev/null
