e0a94ab7a7d11b8eb6e40d51c5a6b691e4a6bb9a
Erik Westrup <erik.westrup@gmail.com>
2018-08-08 18:25:42 +0200

    Collected changes
    Former-commit-id: dc89ffad6581cd2bb47000fcd4762815060464c0
    Former-commit-id: 787d0f3439d9560aaa71afd6b22440c2ccfb3561

---
+++ .shell_functions.d/tmux_set_window_title_to_ssh_host.sh
@@ -0,0 +1,31 @@
+# NOTE stolen and modified version of https://gist.github.com/javipolo/62eb953f817a9a2f63b8127ff5f60788
+# The only downside to this that I've seen so far is that if there are many panes in a single window, the ssh hostname will overwide those. Normall the window title will reflerct the active pane's command.
+
+# Make short hostname only if its not an IP address
+__tm_get_hostname(){
+    local HOST="$(echo $* | rev | cut -d ' ' -f 1 | rev)"
+    if echo $HOST | grep -P "^([0-9]+\.){3}[0-9]+" -q; then
+        echo $HOST
+    else
+        echo $HOST| cut -d . -f 1
+    fi
+}
+
+__tm_get_current_window(){
+    tmux list-windows| awk -F : '/\(active\)$/{print $1}'
+}
+
+# Rename window according to __tm_get_hostname and then restore it after the command
+__tm_command() {
+    if [ "$(ps -p $(ps -p $$ -o ppid=) -o comm=| cut -d : -f 1)" = "tmux" ]; then
+        __tm_window=$(__tm_get_current_window)
+        # Use current window to change back the setting. If not it will be applied to the active window
+        trap "tmux set-window-option -t $__tm_window automatic-rename on 1>/dev/null" EXIT
+        tmux rename-window "$(__tm_get_hostname $*)"
+    fi
+    command "$@"
+}
+
+ssh() {
+    __tm_command ssh "$@"
+}
