5499fcc388f79a09b11e86cb5bda7980d43fc73f
Erik Westrup <erik.westrup@gmail.com>
2019-02-04 19:43:22 +0100

    Collected changes

--- bin/macos_config.sh
+++ bin/macos_config.sh
@@ -188,7 +188,7 @@ EOF
 ## Input Sources
 # * Add US, Swedish & German. Check "Show Input menu in menu bar".
 ## Shortcuts
-# * Go to Input Sources: Enable shortcuts for cycling input sources forward with CTRL+OPT+Space and backwards CTRL+OPT+SHIFT+Space.
+# * Go to Input Sources: Enable shortcuts for cycling input sources _backwards_ with CTRL+OPT+Space. Reason for only having forward is because of keyboard shorcut clash on SHIFT+OPT with Amethyst's cycle layout.
 # * Go to Mission Control:
 # 1. Set shortcut for showing notificatons menu to F13(Print screen).
 # 2. Enable shortcuts Ctrl+[1-5] for switching to Desktops.
