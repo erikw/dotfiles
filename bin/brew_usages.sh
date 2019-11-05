b711188fc5775c4da9e8f21299149a457a1bac1b
Erik Westrup <erik.westrup@gmail.com>
2019-11-05 12:22:17 +0100

    Collected changes
    Former-commit-id: 418cdde12fee05dc0a4dee8fa4498df9ccfd3c21
    Former-commit-id: c25951881d04d8d09a2db9f6ab6986fb8cd7cc3c

--- bin/brew_usages.sh
+++ bin/brew_usages.sh
@@ -2,4 +2,4 @@
 # List brew packages with packages that depends on it. To know which packages that can be removed.
 # Source: https://www.thingy-ma-jig.co.uk/blog/22-09-2014/homebrew-list-packages-and-what-uses-them
 
-brew list -1 | while read cask; do echo -ne "\x1B[1;34m $cask \x1B[0m"; brew uses $cask --installed | awk '{printf(" %s ", $0)}'; echo ""; done
+brew list -1 | while read cask; do printf "%s: " $cask; brew uses $cask --installed | awk '{printf(" %s ", $0)}'; echo ""; done
