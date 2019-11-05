e455e6e2660a6407891e7ecfb20da4d264414869
Erik Westrup <erik.westrup@gmail.com>
2019-11-05 12:22:17 +0100

    Collected changes

--- bin/brew_leafs.sh
+++ bin/brew_leafs.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Show installed brew packages that have no dependencies.
+# Because brew_usages.sh outputs from multiple subprocesses, output needs to be collected completely first.
 
-brew_usages.sh | grep -E "^\s*\w+\s*$"
+
+outf=/tmp/brew_usages.$$.out
+brew_usages.sh 2>&1 >$outf &
+wait
+grep -E "^\s*\w+:\s*$" $outf | tr -d :
