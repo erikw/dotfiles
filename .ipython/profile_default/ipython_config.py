6404449003addbc6b24f193da3be968e2bb7d619
Erik Westrup <erik.westrup@gmail.com>
2019-11-14 17:17:02 +0100

    Collected changes

--- .ipython/profile_default/ipython_config.py
+++
@@ -1,6 +0,0 @@
-# Configuration file for ipython.
-# Generated a default configuration filw with $(ipython3 profile create)
-
-c.TerminalInteractiveShell.editing_mode = 'vi'  # Set vi editing mode.
-c.TerminalIPythonApp.display_banner = False     # Hide start up text.
-c.InteractiveShell.confirm_exit = False         # Don't confirm exits.
