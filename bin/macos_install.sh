f761c62b1b664fca5e6cd14f0bc651eda811a85f
Erik Westrup <erik.westrup@gmail.com>
2020-03-25 12:11:58 +0100

    Collected changes
    Former-commit-id: 446077c8be9f37289b07a426072aac29e29cdff6
    Former-commit-id: 06ca50f5bd3dc71fedabfe14ffdd859a6837926d

--- bin/macos_install.sh
+++ bin/macos_install.sh
@@ -493,14 +493,17 @@ chmod go-w '/usr/local/share'
 #
 #
 #
-# Automator command to cycle output devices:
+# Automator command to cycle output devices. Do this for all ~/bin/macos_media_control/SwitchAudioSource*.command
 # 1. Open automator
 # 2. Create a new service (now named Quick Action)
 # 3. Choose "Run AppleScript"
 # 4. In the top of the window, select for "Service receives selected" to "no input" and "in any application".
-# 5. Paste contents of ~/bin/macos_media_control/sound_output_device_cycle.command
-# 5. Save with the name "cycle_audio_outputs".
-# 6. Open System Peferences>Keyboard>Shortcuts>Services>General and assign cycle_audio_outputs the shortcutl OPT+CMD+F9.
+# 5. Paste contents of ~/bin/macos_media_control/SwitchAudioSource_cycle.command
+# 5. Save with the name "SwitchAudioSource_cycle".
+# 6. Open System Peferences>Keyboard>Shortcuts>Services>General and assign SwitchAudioSource_* to the shortcut
+#  cycle: OPT+CMD+F9.
+#  Built-in: OPT+CMD+F10.
+#  PMA-50: OPT+CMD+F11.
 #
 #
 #
