0b44b674a336bf453ee1e345e7f6912ff34bc609
Erik Westrup <erik.westrup@gmail.com>
2020-03-25 12:11:58 +0100

    Collected changes

--- bin/macos_media_control/SwitchAudioSource.sh
+++ bin/macos_media_control/SwitchAudioSource.sh
@@ -2,10 +2,17 @@
 # Inspired by http://apetronix.com/switch-audio-outputs-with-a-keyboard-shortcut-on-os-x/
 # brew dependencies: switchaudio-osx
 
-set -x
-output=$(/usr/local/bin/SwitchAudioSource -t output -n)
+# Default: cycle devices
+bin=/usr/local/bin/SwitchAudioSource
+cmd="$bin -t output -n"
 
-#output=$(printf "%q" "$output")
+# Output device obtained via $(SwitchAudioSource -a -t output)
+if [ -n "$1" ]; then
+	cmd="$bin -t output -s \"$1\""
+fi
+
+output=$(eval "$cmd")
+
 output=$(echo "$output" | sed -e 's/"//g')
 script=$(printf '%q' "$(basename $0)")
 osascript -e "display notification \"$output\" with title \"$script\""
