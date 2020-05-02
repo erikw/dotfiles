45e45c5dbb79e13c1592e2c5fe69f828c20cb609
Erik Westrup <erik.westrup@gmail.com>
2020-05-02 15:39:01 +0200

    Collected changes
    Former-commit-id: 823664c35cfba7049747187f8d77a2a63a41c7ab
    Former-commit-id: 6e3900e08422ab58227be1b20d2b630207b768f6

--- bin/uberspace_collect_access_log.sh
+++ bin/uberspace_collect_access_log.sh
@@ -3,11 +3,14 @@
 # To be run as a cronjob before midnight where logs are rotated:
 # 0     0     *     *     *      uberspace_collect_access_log.sh
 
-log_src=$HOME/logs/webserver/access_log
+log_today=$HOME/logs/webserver/access_log
+log_yesterday=$HOME/logs/webserver/access_log.1
 log_all=$HOME/logs/www_access_log_all
 
-#cat $log_src >> $log_all
+
+#cat $log_today >> $log_all
+
 # Be sure to only collect new lines AND that we don't miss entries that was added past midnight but before the logs was truncated (around 3am it seems)
 # Reference: https://stackoverflow.com/a/15384897/265508
-touch $log_all
-cat webserver/access_log.1 webserver/access_log | diff -u www_access_log_all - | grep '^\+' | sed -E 's/^\+//' | tail -n +2 >> www_access_log_all
+test -e $log_all || touch $log_all
+cat $log_yesterday $log_today | diff -u $log_all - | grep '^\+' | sed -E 's/^\+//' | tail -n +2 >> $log_all
