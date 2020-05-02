3b769500792f14340e27a98aa48d70dfd22f05e1
Erik Westrup <erik.westrup@gmail.com>
2020-05-02 15:33:48 +0200

    Collected changes
    Former-commit-id: c7d45cb986fe79baf76c5b26209d12bec3f5c37d
    Former-commit-id: 83ed237a6a1de8f72d6701e6abf49406ab70324e

--- bin/uberspace_collect_access_log.sh
+++ bin/uberspace_collect_access_log.sh
@@ -6,4 +6,8 @@
 log_src=$HOME/logs/webserver/access_log
 log_all=$HOME/logs/www_access_log_all
 
-cat $log_src >> $log_all
+#cat $log_src >> $log_all
+# Be sure to only collect new lines AND that we don't miss entries that was added past midnight but before the logs was truncated (around 3am it seems)
+# Reference: https://stackoverflow.com/a/15384897/265508
+touch $log_all
+cat webserver/access_log.1 webserver/access_log | diff -u www_access_log_all - | grep '^\+' | sed -E 's/^\+//' | tail -n +2 >> www_access_log_all
