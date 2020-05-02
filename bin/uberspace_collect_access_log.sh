54b4f57aa7756f42154ab3abcdbdbc2a86a7968f
Erik Westrup <erik.westrup@gmail.com>
2020-05-02 14:42:54 +0200

    Collected changes
    Former-commit-id: c73b2bb91a0cf1c5a3e179c8f967d835e9b3aadd
    Former-commit-id: 40023b031ce7e5befa0b0ef220f02c68c0251363

--- bin/uberspace_collect_access_log.sh
+++ bin/uberspace_collect_access_log.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Concatenate accesslog so I can inspect with goaccess(1).
-# To be run as a cronjob before midnight where logs are rotated.
+# To be run as a cronjob before midnight where logs are rotated:
+# 0     0     *     *     *      uberspace_collect_access_log.sh
 
 log_src=$HOME/logs/webserver/access_log
 log_all=$HOME/logs/www_access_log_all
