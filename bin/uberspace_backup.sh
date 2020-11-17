d3db5514d58b4bf8c50b3e03ebda7e9cb73c26ff
Erik Westrup <erik.westrup@gmail.com>
2020-11-17 22:31:19 +0100

    Collected changes

--- bin/uberspace_backup.sh
+++ bin/uberspace_backup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # Backup my uberspace.de account.
 
-set -x
+set -ex
 
 tar_pattern=takeout_erikw_\*
 bak_dest="$HOME/bak/uberspace/"
 
-ssh uberspace "uberspace takeout takeout"
+# Skip ToolsVersions as of https://github.com/Uberspace/takeout/issues/35
+ssh uberspace "uberspace takeout takeout --skip-item ToolVersions"
 scp uberspace:${tar_pattern} ${bak_dest}
 ssh uberspace "rm ~/${tar_pattern}"
 ls -lh $bak_dest
