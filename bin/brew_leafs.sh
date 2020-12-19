47739df465f5db4095a1fdb0813ab1b5bc637e72
Erik Westrup <erik.westrup@gmail.com>
2020-12-19 21:14:19 +0100

    Collected changes
    Former-commit-id: f3b53c542f37ef1ec8206a15808e5cb1d1351631
    Former-commit-id: 5c71d261e888377b368f17a1785d4d1d32576760

--- bin/brew_leafs.sh
+++ bin/brew_leafs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Show installed brew packages that have no dependencies.
 # Because brew_usages.sh outputs from multiple subprocesses, output needs to be collected completely first.
+# See also $(brew autoremove) to clean up existing dangling formulas!
 
 
 outf=/tmp/brew_usages.$$.out
