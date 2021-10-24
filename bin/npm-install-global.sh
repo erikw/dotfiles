3a52a43243ac25a02ba4b8a05aab2ee3a2b5655e
Erik Westrup <erik.westrup@gmail.com>
2021-10-24 18:31:47 +0200

    Move npm to $XDG
    Former-commit-id: bf7c9d71a03175d05187784463ebbb7a029c6e14
    Former-commit-id: 0db4941cabf8874d3ef2a136031b3de960503361

--- bin/npm-install-global.sh
+++ bin/npm-install-global.sh
@@ -2,7 +2,7 @@
 # Install global NPM packages from a custom requirements file.
 # To list installed packages, run $ npm list -g --depth=0
 
-REQ=$HOME/.npm-global-requirements.txt
+REQ=$XDG_CONFIG_HOME/npm/global-requirements.txt
 if [ -r $REQ ]; then
 	#sed 's/#.*//' $HOME/.npm-global-requirements.txt | xargs npm install -g
 	# Improved support for trailing comments. Reference: https://unix.stackexchange.com/a/384019/19909
