14b661060798fdb4c965c6c4835a26e83b13e1a9
Erik Westrup <erik.westrup@gmail.com>
2022-04-07 19:54:49 +0200

    Collected changes
    Former-commit-id: b715c19eb3143eb0ab067ac4e6332e7070c3e106
    Former-commit-id: 1fc2aea79c3dcd9a09947e3dc7668ec676a1588e

--- bin/largest_files.sh
+++ bin/largest_files.sh
@@ -4,5 +4,6 @@
 
 test -n "$1" && path="$1" || path="."
 
-# Modified version of https://unix.stackexchange.com/a/203705/19909
-find "$path" -type f -print0 | xargs -0 ls -la | awk '{print int($5/(1024^2)) " MiB\t" $9}' | sort -n -r -k1
+# Modified version of https://unix.stackexchange.com/a/203705/19909 that handles file names with spaces.
+# Ref: https://linuxhint.com/print-columns-awk/
+find "$path" -type f -print0 | xargs -0 ls -la | awk '{for (i=5; i<NF; i++) { if (i == 5) { printf int($i/(1024^2)) " MiB\t" } else if (i >= 9 ) {  printf $i " " } }; if (NF >= 5) print $NF; }' | sort -n -r -k1
