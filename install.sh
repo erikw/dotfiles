c270e6475cd028ab483c20c2bd3a9f152f43c1c6
Erik Westrup <erik.westrup@gmail.com>
2022-05-25 09:31:02 +0200

    Collected changes
    Former-commit-id: 47807a6701c32ed7b216596c4ff42520bc86815b
    Former-commit-id: 24baf85657ea7c72b9dbb3b1d31229b432a0c936

--- install.sh
+++ install.sh
@@ -18,11 +18,11 @@ cd $BASEDIR
 source .config/shell/functions
 source .config/shell/functions.d/sourceifexists.sh
 
-step "clone git submodules"
-git submodule init
-git submodule update
 
 step "Setting up git"
+# For non-interactive setups e.g. GitHubCoderspaces. Needed as git submodule init asks about github.com fingerprint.
+ssh-keyscan github.com ~/.ssh/known_hosts
+
 # NOTE disabled as this script should work as non-interactive e.g. GitHub Codespaces VSCode setup.
 #email=
 #while [ -z "$email" ]; do
@@ -37,6 +37,10 @@ git remote add upstream git@github.com:justone/dotfiles.git
 git submodule init
 git submodule update
 git checkout -b local
+
+step "clone git submodules"
+git submodule init
+git submodule update
 
 step "Installing dotfiles with dfm"
 bin/dfm install
