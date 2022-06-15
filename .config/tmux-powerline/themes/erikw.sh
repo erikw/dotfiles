5393c5f44964c9aa0d880fccc631d0538bd411dc
Erik Westrup <erik.westrup@gmail.com>
2022-06-15 11:44:23 +0200

    Collected changes
    Former-commit-id: 77f9d2af605e3d6a206c1cc5cad39f150b765c9c
    Former-commit-id: bf03c7f70afd82998dc6ed7f179cec60193bf439

--- .config/tmux-powerline/themes/erikw.sh
+++ .config/tmux-powerline/themes/erikw.sh
@@ -66,6 +66,7 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
 		"pwd 89 211" \
+		"now_playing" \
 		"weather" \
 		#"macos_notification_count 29 255" \
 		#"cpu 240 136" \
