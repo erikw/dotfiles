8de0b55f5ea90b841ee9cad35424ca6da32bafbe
Erik Westrup <erik.westrup@gmail.com>
2022-11-02 14:54:51 +0100

    Collected changes
    Former-commit-id: 4888bf44f052073b4ccabaf6eb3cda8d38468962
    Former-commit-id: 7d06c37537bdcd8f7acb63244e58c69c56122d0d

--- .config/tmux-powerline/themes/erikw.sh
+++ .config/tmux-powerline/themes/erikw.sh
@@ -53,7 +53,7 @@ fi
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		"tmux_session_info 148 234" \
-		"hostname 33 0" \
+		#"hostname 33 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
 		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
