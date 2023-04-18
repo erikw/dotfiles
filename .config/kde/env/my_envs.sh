1d8723c4abf1f6b833b0a040b13150fd0b208bf8
Erik Westrup <erik.westrup@gmail.com>
2023-04-18 18:06:10 +0200

    Cleanup .config/
    Former-commit-id: f9fa326babca6bfbcdd09bc96e4b96925d874e77
    Former-commit-id: 1aff232cc6a37c863f73cedcf6dc1985b84c98d2

--- .config/kde/env/my_envs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# KDE4 environment variables.
-
-# Tell gPodder to reside in multimedia directory.
-export GPODDER_HOME="$HOME/media/music/gPodder"
-
-# Let programs know where to connect to MPD.
-export MPD_HOST="somepasswordwhenIhaveaKDEmachineagain@localhost"
-export MPD_PORT="6600"
--- .config/mpd/env.sh
+++
@@ -1,6 +0,0 @@
-# Source this file where MPD variables are needed e.g. ~/.bashrc.
-
-# Password for conncting to MPD throuh mpc (and others).
-export MPD_HOST="GIT-CENSORED@localhost"
-# MPD port to use.
-export MPD_PORT="6600"
