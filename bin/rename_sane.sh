885448a5fc2ee841623f77e60ba461afcc88c233
Erik Westrup <erik.westrup@gmail.com>
2023-04-25 20:40:55 +0200

    Get rid of cpan with detox
    Former-commit-id: 2a0f535a6ad93d3265f458361e1c38abee9ec2c5
    Former-commit-id: 6cfa2cd623acbba30fa9c28cdfb8cfbc9dcaddd5

--- bin/rename_sane.sh
+++ bin/rename_sane.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env sh
 # Rename to sane file names.
-# * Requirements: rename(.pl)
-# * Requirements: $ cpan Text::Unidecode
-#   * non-interactive: $ PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Text::Unidecode'
+# Usage: rename_sane.sh [path]
+# Requirements:
+# - rename(.pl)
+# - Requirements: detox
+# - ~Requirements: $ cpan Text::Unidecode~
+#   - non-interactive: $ PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'install Text::Unidecode'
 
 test -n "$1" && path="$1" || path="."
 
 IFS=$'\n'
 for dir in $(find $path -type d | tac); do
+	test -z "$(ls -A "$dir")" && continue
+
 	cd "$dir" && echo "====> Entering: $dir/"
-        # --force: for case-insensitive fs https://github.com/ap/rename/issues/16
+    # --force: for case-insensitive fs https://github.com/ap/rename/issues/16
 	rename --verbose --transcode utf8 --lower-case --sanitize --force  *
-	# Recode Unicode to ASCII. See rename(1).
-	rename --verbose --transcode utf8 -MText::Unidecode '$_ = unidecode $_' *
+
+	# NOPE Recode Unicode to ASCII. See rename(1).
+	#rename --verbose --transcode utf8 -MText::Unidecode '$_ = unidecode $_' *
+	# Better use detox, so that we don't need to install cpan modules!
+	detox -rv  .
+
 	cd - >/dev/null
 done
