ce1f387fe632e8201046eef51642b1767f7b1579
Erik Westrup <erik.westrup@icloud.com>
2025-12-01 19:33:57 +0100

    Collected changes

--- bin/macos_config.sh
+++ bin/macos_config.sh
@@ -112,8 +112,8 @@ defaults write com.apple.dock wvous-tr-modifier -int 0
 # * Bottom Left corner: Desktop
 defaults write com.apple.dock wvous-bl-corner -int 4
 defaults write com.apple.dock wvous-bl-modifier -int 0
-# * Bottom Right corner: Put Display to Sleep
-defaults write com.apple.dock wvous-br-corner -int 10
+# * Bottom Right corner: NOP
+defaults write com.apple.dock wvous-br-corner -int 0
 defaults write com.apple.dock wvous-br-modifier -int 0
 # }
 
